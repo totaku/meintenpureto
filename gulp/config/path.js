0e9aad7e7533455a4984786158474477d8ceb10c
Totaku <banana@3lf.ru>
2022-04-08 02:08:26 +0300

    task fonts

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,21 +10,20 @@ export const path = {
         css: buildFolder + '/css/',
         js: buildFolder + '/js/',
         images: buildFolder + '/img/',
-        files: buildFolder + '/files/',
+        fonts: buildFolder + '/fonts/'
     },
     src: {
         html: srcFolder + '/*.html',
         scss: srcFolder + '/scss/style.scss',
         js: srcFolder + '/js/app.js',
         images: srcFolder + '/img/**/*.{jpg,jpeg,png,gif,webp,svg}',
-        files: srcFolder + '/files/**/*.*',
+        fonts: srcFolder + '/fonts/*.{eot,ttf,otc,ttc,woff,woff2,otf,svg}'
     },
     watch: {
         html: srcFolder + '/**/*.html',
         scss: srcFolder + '/scss/**/*.scss',
         js: srcFolder + '/js/**/*.js',
         images: srcFolder + '/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}',
-        files: srcFolder + '/files/**/*.*',
     },
     clean: buildFolder,
     buildFolder: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ import gulp from 'gulp';
 
 import { path } from './gulp/config/path.js';
 import { plugins } from './gulp/config/plugins.js';
-import { copy } from './gulp/tasks/copy.js';
 import { clean } from './gulp/tasks/clean.js';
 import { html } from './gulp/tasks/html.js';
 import { server } from './gulp/tasks/server.js';
 import { scss } from './gulp/tasks/scss.js';
 import { js } from './gulp/tasks/js.js';
 import { images } from './gulp/tasks/images.js';
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
 
 global.app = {
     gulp: gulp,
@@ -17,14 +17,15 @@ global.app = {
 }
 
 function watcher() {
-    gulp.watch(path.watch.files, copy)
     gulp.watch(path.watch.html, html)
     gulp.watch(path.watch.scss, scss)
     gulp.watch(path.watch.js, js)
     gulp.watch(path.watch.images, images)
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
+
+const mainTasks = gulp.series(fonts, gulp.parallel(html, scss, js, images));
 
 const dev = gulp.series(clean, mainTasks, gulp.parallel(watcher, server));
 
